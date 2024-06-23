281512a95357befbd63aec47b3399a11d54b9b56
Ebowwa <ec.arbee1@gmail.com>
2024-06-23 13:15:36 -0700

    update

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,23 +1,25 @@
-export const MAX_IMAGE_SIZE = 1024 * 1024; // 1MB
-export const MAX_IMAGES=4
-export const EXAMPLE_PATH = "gemini-pro-vision-playground";
-export const CMS_NAME = "Markdown";
+// src/lib/constants/index.ts
+import { constants_keywords } from "./keywords";
+// export const MAX_IMAGE_SIZE = 1024 * 1024; // 1MB
+// export const MAX_IMAGES=4
+// export const EXAMPLE_PATH = "gemini-pro-vision-playground";
+export const CMS_NAME = "un-automated";
 export const HOME_OG_IMAGE_URL = "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
-export const NEXT_SITE_TITLE = "Next.js Blog Starter";
-export const NEXT_SITE_DESCRIPTION = "A blog starter built with Next.js and Markdown";
+export const NEXT_SITE_TITLE = "un-automated";
+export const NEXT_SITE_DESCRIPTION = "Un-Automated.com: Empowering individuals to turn their passions into income, with tools and resources to help anyone start selling online, regardless of their background or experience.";
 export const cardImage = HOME_OG_IMAGE_URL;
 export const robots = "follow, index";
-export const favicon = "/favicon.ico";
+export const favicon = "/favicon.ico"; //https://favicon.io/favicon-converter/
 
 export const referrer = 'origin-when-cross-origin';
-export const keywords = ['Vercel', 'Supabase', 'Next.js', 'Stripe', 'Subscription', CMS_NAME];
-export const authors = [{ name: 'Vercel', url: 'https://vercel.com/' }];
-export const creator = 'Vercel';
-export const publisher = 'Vercel';
+export const keywords = [...constants_keywords,'Vercel', 'Supabase', 'Next.js', 'Stripe', 'Subscription', CMS_NAME];
+export const authors = [{ name: 'Ebowwa', url: 'https://x.com/innitEBOWWA' }];
+export const creator = 'Ebowwa';
+export const publisher = 'Ebowwa';
 
 export const twitterCard = 'summary_large_image';
-export const twitterSite = '@Vercel';
-export const twitterCreator = '@Vercel';
+export const twitterSite = '@innitEBOWWA';
+export const twitterCreator = '@innitEBOWWA';
 export const ogType = 'website';
 
 export const route = {
