d00d72723f4edb0a17f29d04e24a7d87c5fd8dea
Ebowwa <ec.arbee1@gmail.com>
2024-06-29 23:04:40 -0700

    switched default theme from dark to light

--- src/utils/storage/provider/theme/index.tsx
+++ src/utils/storage/provider/theme/index.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from "react";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       {children}
     </ThemeProvider>
   );
